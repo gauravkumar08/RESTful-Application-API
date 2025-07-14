28d74cc88785b65327562cca812c5bb3fa8bba33
Gaurav Kumar <115710118+gauravkumar08@users.noreply.github.com>
2025-07-14 13:43:44 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri);
     console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
