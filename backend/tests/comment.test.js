bce019bdd31975c6896ab8148ac99346a28d5767
Gaurav Kumar <115710118+gauravkumar08@users.noreply.github.com>
2025-07-18 15:35:05 +0530

    comment.test.js

--- backend/tests/comment.test.js
+++ backend/tests/comment.test.js
@@ -1,12 +1,26 @@
-const request = require("supertest");
-const app = require("../server");
+const request = require('supertest');
+const app = require('../server'); 
+const { connect, clearDatabase, closeDatabase } = require('./setup');
 
-describe("POST /api/comments", () => {
-  it("should return 401 if no token is provided", async () => {
-    const res = await request(app).post("/api/comments").send({
-      postId: "507f1f77bcf86cd799439011",
-      content: "Test comment",
-    });
-    expect(res.statusCode).toEqual(401);
+beforeAll(async () => {
+  await connect();
+});
+
+afterEach(async () => {
+  await clearDatabase();
+});
+
+afterAll(async () => {
+  await closeDatabase();
+});
+
+describe('POST /api/comments', () => {
+  test('should return 401 if no token is provided', async () => {
+    const res = await request(app)
+      .post('/api/comments')
+      .send({ postId: '123', content: 'Test comment' });
+
+    expect(res.statusCode).toBe(401);
   });
+
 });
