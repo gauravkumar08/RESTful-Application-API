09dff4420132a12ea526882b2ad34d64f0c5ded1
Gaurav Kumar <115710118+gauravkumar08@users.noreply.github.com>
2025-07-18 15:41:36 +0530

    postController.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,35 +1,62 @@
 const Post = require("../models/Post");
 
 exports.createPost = async (req, res) => {
-  const post = new Post({ ...req.body, author: req.user.id });
-  await post.save();
-  res.status(201).json(post);
+  try {
+    const post = new Post({ ...req.body, author: req.user.id });
+    await post.save();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message || "Internal server error" });
+  }
 };
 
 exports.getPosts = async (req, res) => {
-  const posts = await Post.find().populate("author", "username");
-  res.json(posts);
+  try {
+    const posts = await Post.find().populate("author", "username");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.getPost = async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post) return res.status(404).json({ message: "Post not found" });
-  res.json(post);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post)
+      return res.status(404).json({ message: "Post not found" });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.updatePost = async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post || post.author.toString() !== req.user.id)
-    return res.status(403).json({ message: "Not allowed" });
-  Object.assign(post, req.body);
-  await post.save();
-  res.json(post);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post)
+      return res.status(404).json({ message: "Post not found" });
+    if (post.author.toString() !== req.user.id)
+      return res.status(403).json({ message: "Not allowed" });
+
+    Object.assign(post, req.body);
+    await post.save();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.deletePost = async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post || post.author.toString() !== req.user.id)
-    return res.status(403).json({ message: "Not allowed" });
-  await post.remove();
-  res.json({ message: "Post deleted" });
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post)
+      return res.status(404).json({ message: "Post not found" });
+    if (post.author.toString() !== req.user.id)
+      return res.status(403).json({ message: "Not allowed" });
+
+    await post.deleteOne(); 
+    res.status(200).json({ message: "Post deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
